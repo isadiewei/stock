47a94eb69c6fbf73a1b4b99d74f618c05323927e
Halt, begrudged <isadiewei@pm.me>
2025-01-18 12:49:43 -0500

    - removed old data frame file

--- src/components/DataFrame/index.tsx
+++
@@ -1,2 +0,0 @@
-export { DataFrame as CatchDataFrame } from '../../pages/Catch/CatchDashboard/DataFrame/DataFrame.component';
-// export { DataFrame as FishDataFrame } from './Fish/DataFrame.component';
